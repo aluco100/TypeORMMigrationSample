7bdc7b71f2d916b46471b1263879e34b45bd3f1e
Alfredo Luco <aluco100@gmail.com>
2018-01-11 11:27:45 -0300

    init

---
+++ src/migration/1515622226541-Test.ts
@@ -0,0 +1,17 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class Test1515622226541 implements MigrationInterface {
+
+  async up(queryRunner: QueryRunner): Promise<any> {
+      await queryRunner.renameColumn("post", "title", "name");
+      // alternatively you can do:
+      // await queryRunner.query("ALTER TABLE `post` CHANGE `title` `name` VARCHAR(255)");
+  }
+
+  async down(queryRunner: QueryRunner): Promise<any> {
+      await queryRunner.renameColumn("post", "name", "title");
+      // alternatively you can do:
+      // await queryRunner.query("ALTER TABLE `post` CHANGE `name` `title` VARCHAR(255)");
+  }
+
+}
